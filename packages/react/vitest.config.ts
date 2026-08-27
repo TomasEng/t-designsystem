6ebb651c47977e2b12c563f2a6086f46a85155da
Tomas <tomas.engebretsen@gmail.com>
2026-08-27 13:28:32 +0200

    Fikse kodestil

--- packages/react/vitest.config.ts
+++ packages/react/vitest.config.ts
@@ -14,6 +14,6 @@ export default defineConfig({
     },
     coverage: {
       enabled: true,
-    }
+    },
   },
 });
--- packages/web/vitest.config.ts
+++ packages/web/vitest.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
       instances: [{ browser: "chromium" }],
     },
     coverage: {
-      enabled: true
-    }
+      enabled: true,
+    },
   },
 });
